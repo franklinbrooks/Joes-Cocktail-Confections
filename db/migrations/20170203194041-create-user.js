892747248fc188dcf6401fb286b5516e9f08eff7
Dominic Farquharson <farquharsondominic@gmail.com>
2017-02-03 14:44:51 -0500

    User model

---
+++ db/migrations/20170203194041-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_name: {
+        type: Sequelize.STRING(255)
+      },
+      password: {
+        type: Sequelize.TEXT
+      },
+      first_name: {
+        type: Sequelize.STRING
+      },
+      last_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING(128)
+      },
+      cell_number: {
+        type: Sequelize.BIGINT
+      },
+      dob: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Users');
+  }
+};
