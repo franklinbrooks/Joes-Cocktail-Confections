7dac23b5fc81aea94e91eee0fbd6c6ad8736a459
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-02-03 14:54:00 -0500

    Linked auth routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var path = require('path');
+const authHelpers = require('../auth/auth-helpers');
+const passport = require('../auth/local');
 
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
