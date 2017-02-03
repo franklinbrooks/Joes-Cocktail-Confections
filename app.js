66aff9cebb5d6e3eb52262bee497d1e95bce48eb
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-02-03 15:05:06 -0500

    fixed appjs passport issue

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-const authHelpers = require('../auth/auth-helpers');
-const passport = require('../auth/local');
 
 // var favicon = require('serve-favicon');
 var logger = require('morgan');
