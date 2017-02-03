8c471cbfa7224426d038b4a02ca6045db2534096
Dominic Farquharson <farquharsondominic@gmail.com>
2017-02-03 12:51:36 -0500

    Updated index.js w/ sequelize variable, Added Orders Model

---
+++ db/migrations/20170203174645-create-orders.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.BIGINT
+      },
+      quantity: {
+        type: Sequelize.BIGINT
+      },
+      delivery_date: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Orders');
+  }
+};
