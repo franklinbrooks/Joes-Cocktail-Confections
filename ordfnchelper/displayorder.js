703626a24e2245a7ff6c9ca56bc71846c9f30fcd
Shantel Moore <moore.shantel@gmail.com>
2017-02-05 11:54:32 -0500

    category ejs and routes

--- ordfnchelper/displayorder.js
+++ ordfnchelper/displayorder.js
@@ -1,6 +1,6 @@
 const models = require('../db/models/index');
 
-function getOrders(req,res,next) {
+function getOrders(req, res, next) {
   models.sequelize.query('SELECT "Products"."product_name", "Products"."category", "Products"."price", "Products"."unit_size", "Products"."alcohol", "User"."name" FROM"Users" JOIN "Orders" ON "Users"."id" = "Orders"."user_id"JOIN "Products" ON "Products"."id" = "Orders"."product_id" WHERE "Users"."id" = :id', {
     replacements: { id: req.user.id }, /// replaces :id in the query
     type: models.sequelize.QueryTypes.SELECT // don't need metadata in the response
--- routes/products.js
+++ routes/products.js
@@ -1,35 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../db/models/index');
+
+const authHelpers = require('../auth/auth-helpers');
+const displayCupcakes = require('../ordfnchelper/displaycupcakes')
 
 /* GET products listing. */
-router.get('/cupcakes', function(req, res, next) {
-  res.render('products/cupcakes', {
-    title: "Joe's Cocktail Confections - Cupcake Index"
+
+router.get('/cupcakes', function(req, res, next) {  // main route
+ models.Product.findAll({
+    where: { category: "Cupcakes"}
+  }).then(function(cupcakes){
+    res.render('products/cupcakes', {
+      title: "Cupcakes - Joe's Cocktail Confections",
+      cupcakes: cupcakes
+    });
   });
 });
 
-/*router.get('/', function(req, res, next) {  // main route
-  models.Products.findAll({}).then(function(products){
-    res.render('products/cupcakes', {
-      title: "Joe's Cocktail Confections - Cupcake Index",
-      products: products
-    });
-  });
-});*/
-
 router.get('/cakes', function(req, res, next) {
-  res.render('products/cakes', {
-    title: "Joe's Cocktail Confections - Cake Index"
+  models.Product.findAll({
+    where: { category: "Cakes"}
+  }).then(function(cakes){
+    res.render('products/cakes', {
+    title: "Cakes - Joe's Cocktail Confections",
+    cakes: cakes
+   });
   });
 });
 
 router.get('/strawberries', function(req, res, next) {
+  models.Product.findAll({
+    where: { category: "Strawberries"}
+  }).then(function(strawberries){
   res.render('products/strawberries', {
-    title: "Joe's Cocktail Confections - Strawberries Index"
+    title: "Strawberries - Joe's Cocktail Confections",
+    strawberries: strawberries
   });
+ });
 });
 
-router.get('/item', function(req, res, next) {
+router.get('/item/:id', function(req, res, next) {
   res.render('products/item', {
     title: "Joe's Cocktail Confections - Item Detail"
   });
