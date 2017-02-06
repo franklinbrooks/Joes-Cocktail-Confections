dd7108cb8e604e43c58453f4c606e467dd21c25d
Dominic Farquharson <farquharsondominic@gmail.com>
2017-02-06 17:00:57 -0500

    Adding conditional rendering for header

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // setting username variable to null to allow for conditional rendering
+  const username = null;
   res.render('index', {
-    title: "Joe's Cocktail Confections Home"
+    title: "Joe's Cocktail Confections Home",
+    username:username
   });
 });
 
