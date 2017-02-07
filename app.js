78f16451166038ef9408ccc8fada39cfe365e62b
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-02-07 15:04:22 -0500

    Cleaned code

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+// Middleware and dependencies
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
+
+// Routes
 const index = require('./routes/index');
 const authRoutes = require('./routes/auth.js');
 const userRoutes = require('./routes/user.js');
@@ -15,21 +17,19 @@ const order = require('./routes/order');
 const links = require('./routes/links');
 const app = express();
 const methodOverride = require('method-override'); // for edit/delete
-// load environment variables
+
+// Environment variables
 require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// override with POST having ?_method=DELETE
+// .use methods
 app.use(methodOverride('_method'));  // for edit/delete
-
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(cookieParser());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
@@ -39,7 +39,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
@@ -57,15 +56,6 @@ app.use('/products', products);
 app.use('/order', order);
 app.use('/links', links);
 
-
-
-/*
-app.use(function (req, res, next) {
-  res.render('error', {
-    title: "Joe's Cocktail Confections - Error"
-  });
-});
-*/
 // Handle 404 error page both error pages need to be at bottom of app.js
 app.use(function(req, res) {
   let username = null;
@@ -79,7 +69,7 @@ app.use(function(req, res) {
   });
 });
 
-  // Handle 500 error page
+// Handle 500 error page
 app.use(function(error, req, res, next) {
   let username = null;
   if (req.user) {
@@ -92,23 +82,5 @@ app.use(function(error, req, res, next) {
     username: username
   });
 });
-
-
-/* Below error handling routes replaced by custom 404 route above */
-// catch 404 and forward to error handler
-/*app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});*/
-// error handler
-/*app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('/error');
-});*/
 
 module.exports = app;
